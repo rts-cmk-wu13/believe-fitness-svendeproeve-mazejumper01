4407559b9f5c4d45b148264ad4995d49c902f7a2
Magnus <mazejumper01@gmail.com>
2026-03-09 13:45:52 +0100

    newsletter er nu stylet korrekt

--- src/components/nyhedsform/nyhedsForm.jsx
+++ src/components/nyhedsform/nyhedsForm.jsx
@@ -17,31 +17,35 @@ export default function NyhedsForm() {
     )
 
     return (
-        <form action={formAction} className="flex flex-col  gap-2">
-            
-            <div className="flex gap-2 items-center">
-                <input
-                    type="email"
-                    name="email"
-                    placeholder="Indtast din email"
-                    defaultValue={state.values.email}
-                    className="max-w-63.75 h-11.25 bg-white text-black p-2"
-                />
+        <section className="pt-10 flex flex-col gap-5">
+            <h3 className="info-h3">Sign up for our newsletter</h3>
+            <p>Sign up to receive the latest news and announcements from Believe Fitness</p>
+            <form action={formAction} className="flex flex-col  gap-2">
+                
+                <div className="flex gap-5 items-center">
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Enter your email"
+                        defaultValue={state.values.email}
+                        className="max-w-63.75 h-11.25  rounded-full border-gray-400 border focus:outline-none focus:ring-0 p-2"
+                    />
 
-                            <button
-                    type="submit"
-                    disabled={isPending}
-                    className=" w-20.75 h-13.25 bg-gray-300 text-black rounded disabled:opacity-50"
-                >
-                    {isPending ? "Tilmeldes..." : "Tilmeld"}
-                </button>
-            </div>
+                                <button
+                        type="submit"
+                        disabled={isPending}
+                        className=" btn w-27 h-13.25 text-black rounded disabled:opacity-50"
+                    >
+                        {isPending ? "signing up..." : "signup"}
+                    </button>
+                </div>
 
-            {state.errors?.email && <p>{state.errors.email}</p>}
-            {state.errors?.form && <p>{state.errors.form}</p>}
-            {state.success && <p>{state.success}</p>}
+                {state.errors?.email && <p>{state.errors.email}</p>}
+                {state.errors?.form && <p>{state.errors.form}</p>}
+                {state.success && <p>{state.success}</p>}
 
 
-        </form>
+            </form>
+        </section>
     )
 }
